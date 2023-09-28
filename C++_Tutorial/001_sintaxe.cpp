4560f29cf3095d76f14da42815cbfa64d03f57bf
Vicrrs <enricofermi2019@gmail.com>
2023-09-28 02:36:27 -0400

    sintaxe

---
+++ C++_Tutorial/001_sintaxe.cpp
@@ -0,0 +1,8 @@
+// biblioteca de arquivos do cabecalho que permite a trabalhar com objetos de entrada e saida.
+#include <iostream>
+// significa que podemos usar nomes para objetos e variaveis da biblioteca padrao
+using namespace std;
+int main() {
+	cout << "Ola C++" << endl;
+	return 0;
+}
