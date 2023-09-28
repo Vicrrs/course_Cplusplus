6a90fd291dc08ba73ad9546ebdc9801fc6ba9dcd
Vicrrs <enricofermi2019@gmail.com>
2023-09-28 02:40:09 -0400

    output new line

---
+++ C++_Tutorial/003_output_nl.cpp
@@ -0,0 +1,10 @@
+// New Line, inserindo nova linha em branco
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello c++! \n";
+	cout << "I am Learning C++ \n\n";
+	cout << "I love python" << endl;
+}
