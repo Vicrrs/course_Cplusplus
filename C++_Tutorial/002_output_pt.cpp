6de0563082b8895a22213c662582df486e182cbd
Vicrrs <enricofermi2019@gmail.com>
2023-09-28 02:38:34 -0400

    output print text

---
+++ C++_Tutorial/002_output_pt.cpp
@@ -0,0 +1,10 @@
+// Print Text, o cout objeto, junto com o << operador, eh usado para gerar valores / imprimir texto
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello C++!" << endl;
+	cout << "I am learning C++" << endl;
+	return 0;
+}
