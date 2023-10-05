40818d9be49b33db207556323ec0117f3b93f292
Vicrrs <enricofermi2019@gmail.com>
2023-10-05 00:15:50 -0400

    vartypes

---
+++ C++_Tutorial/005_varTypes.cpp
@@ -0,0 +1,9 @@
+/*
+int - armazenas numeros inteiros, sem decimais, como 123 ou -123
+double - armazena numeros de ponto flutuante, com casas decimais, como 19.99 ou -19.99
+char - armazena caracteres unicos, como 'a' ou 'B'.
+string - armazena teste, "Hello c++".
+bool - armazena valores com dois estados: true or false.
+
+-> Para criar uma variavel, especifique o tipo e atribua um valor a ela:
+ */
