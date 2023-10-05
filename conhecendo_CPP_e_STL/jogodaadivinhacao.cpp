e82df21add3ffad0b2524eb89f1e675e53d0da0c
Vicrrs <enricofermi2019@gmail.com>
2023-10-05 00:16:31 -0400

    execucao condicional

--- conhecendo_CPP_e_STL/jogodaadivinhacao.cpp
+++ conhecendo_CPP_e_STL/jogodaadivinhacao.cpp
@@ -3,12 +3,26 @@ using namespace std;
 
 int main (){
 	cout <<"**********************************"<< endl;
-	cout <<"* Bem-vidos ao jogo de adivinhação"<< endl;
+	cout <<"*Bem-vidos ao jogo de adivinhação*"<< endl;
 	cout <<"**********************************"<< endl;
 
 	int numero_secreto = 42;
 	// Operador de insercao
-	cout << "O número secreto é " <<numero_secreto<<", nao conte para ninguém!" << endl;
+	// cout << "O número secreto é " <<numero_secreto<<", nao conte para ninguém!" << endl;
 
-	cin >> 
+	int chute;
+	cout << "Qual seu chute? ";
+	cin >> chute;
+	cout << "O valor do seu chute é: " << chute << endl;
+
+	if(chute == numero_secreto){
+		cout << "Parabéns, você acertou o número secreto!" << endl;
+	}
+	else if(chute >  numero_secreto){
+		cout << "Seu chute foi maior que o numero secreto!" << endl;
+	}
+	else{
+		cout << "Seu chute foi menor que o número secreto!" << endl;
+	};
+
 }
