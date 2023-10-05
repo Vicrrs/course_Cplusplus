f9efddcd760c376b64a3bc09334bb1a93ee4c300
Vicrrs <enricofermi2019@gmail.com>
2023-10-05 00:37:34 -0400

    valores booleanos e declarando constante

--- conhecendo_CPP_e_STL/jogodaadivinhacao.cpp
+++ conhecendo_CPP_e_STL/jogodaadivinhacao.cpp
@@ -6,19 +6,23 @@ int main (){
 	cout <<"*Bem-vidos ao jogo de adivinhação*"<< endl;
 	cout <<"**********************************"<< endl;
 
-	int numero_secreto = 42;
+	// constantes em caps lock
+	const int NUMERO_SECRETO = 42;
 	// Operador de insercao
 	// cout << "O número secreto é " <<numero_secreto<<", nao conte para ninguém!" << endl;
 
 	int chute;
-	cout << "Qual seu chute? ";
-	cin >> chute;
+	cout << "Qual seu chute? "; // print
+	cin >> chute; // input
 	cout << "O valor do seu chute é: " << chute << endl;
 
-	if(chute == numero_secreto){
+	bool acertou = chute == NUMERO_SECRETO;
+	bool maior = chute > NUMERO_SECRETO;
+
+	if(acertou){
 		cout << "Parabéns, você acertou o número secreto!" << endl;
 	}
-	else if(chute >  numero_secreto){
+	else if(maior){
 		cout << "Seu chute foi maior que o numero secreto!" << endl;
 	}
 	else{
