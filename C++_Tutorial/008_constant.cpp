7fe76a77dabffbec3217ca589c4281b21ada630e
vicrrs <enricofermi2019@gmail.com>
2023-11-02 15:46:46 -0400

    constants in cpp

---
+++ C++_Tutorial/008_constant.cpp
@@ -0,0 +1,11 @@
+/*
+Quando você não quer que os outros (ou você mesmo) alterem os valores das variáveis existentes, use o constpalavra-chave (isso irá declarar o variável como "constante", o que significa imutável e somente leitura)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int minutesPerHour = 60;
+    const float PI = 3.14;
+}
