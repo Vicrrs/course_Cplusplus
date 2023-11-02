1ca4333dcb82eb684e08b93db8d5c7ab45b36346
vicrrs <enricofermi2019@gmail.com>
2023-11-02 15:46:39 -0400

    identifiers in cpp

---
+++ C++_Tutorial/007_Identifiers_cpp.cpp
@@ -0,0 +1,17 @@
+/*
+As regras gerais para as variáveis de nomeação são:
+
+    Os nomes podem conter letras, dígitos e sublinhados
+    Os nomes devem começar com uma letra ou um sublinhado (_)
+    Os nomes são sensíveis a casos ( myVarE a myvarSão variáveis diferentes)
+    Os nomes não podem conter espaços em branco ou caracteres especiais como !, %, etc.
+    Palavras reservadas (como palavras-chave C++, como int) não pode ser utilizado como nomes.
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int minutesPerHour = 60;
+    cout << minutesPerHour << endl;
+}
+
