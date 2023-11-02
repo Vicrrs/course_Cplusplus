886b3608e855b2870a37b6416f8af24fa6c1e316
vicrrs <enricofermi2019@gmail.com>
2023-11-02 15:46:27 -0400

    multiple variables in cpp

---
+++ C++_Tutorial/006_MultipleVariable.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 4, b = 8, c = 30;
+    int x, y, z;
+    x = y = z = 50;
+    cout << x + y + z << endl;
+    cout << a + b + c << endl;
+    return 0;
+}
