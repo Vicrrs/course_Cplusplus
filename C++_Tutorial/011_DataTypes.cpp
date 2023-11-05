5f04c1c880bffa8ea22086b41dce29bea08e13cb
vicrrs <enricofermi2019@gmail.com>
2023-11-05 10:36:02 -0400

    data types

---
+++ C++_Tutorial/011_DataTypes.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int myNum = 5;
+    float myFolatNum = 5.99;
+    double myDoubleNum = 9.98;
+    char myLetter = 'H';
+    bool myBoolean = true;
+    string myText = "Hello";
+}
