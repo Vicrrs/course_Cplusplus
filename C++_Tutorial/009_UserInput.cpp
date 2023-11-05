8e7cd61450c1ede6d191b8f9688f6e762bc4592e
vicrrs <enricofermi2019@gmail.com>
2023-11-05 02:16:38 -0400

    input in cpp

---
+++ C++_Tutorial/009_UserInput.cpp
@@ -0,0 +1,15 @@
+/*
+cout é pronunciado "ver-out". Usado para saída e usa o operador de inserção (<<) - Em que é)
+
+cin é pronunciado "see-in". Usado para entrada e usa o operador de extração (>>) - Em que é)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cout << "Type Number: ";
+    cin >> x;
+    cout << "Your number is: " << x;
+}
